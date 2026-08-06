2d19e5bbf0d548db81f99859ca992ef48220801b
Rajeev <rajeevhaldar8265@gmail.com>
2026-08-06 10:53:09 +0530

    Support POST method on /:id/sign route

--- routes/petitionRoutes.js
+++ routes/petitionRoutes.js
@@ -42,7 +42,7 @@ router
   .put(protect, upload.array("images", 4), updatePetition)
   .delete(protect, deletePetition);
 
-router.route("/:id/sign").put(protect, signPetition);
+router.route("/:id/sign").put(protect, signPetition).post(protect, signPetition);
 router.route("/:id/check-signature").get(protect, checkUserSignature);
 router.route("/:id/signers").get(protect, getPetitionSigners);
 
